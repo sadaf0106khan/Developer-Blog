af8684c7dec06087747e3cd3d227f3765c24dc0e
sadaf khan <sadaf@goodeducator.com>
2021-03-17 19:16:44 +0530

    Updates

---
+++ precache-manifest.56a2ba9d48b9768daadd942057af3bdd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d3f6685a29857b455a9f516a9d8b0e8",
+    "url": "/Developer-Blog/index.html"
+  },
+  {
+    "revision": "be1de6d0b75eeb003664",
+    "url": "/Developer-Blog/static/css/2.5529d428.chunk.css"
+  },
+  {
+    "revision": "a8e784cfbe94b6d87ae3",
+    "url": "/Developer-Blog/static/css/main.5cdc44f1.chunk.css"
+  },
+  {
+    "revision": "be1de6d0b75eeb003664",
+    "url": "/Developer-Blog/static/js/2.23af44e7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Developer-Blog/static/js/2.23af44e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8e784cfbe94b6d87ae3",
+    "url": "/Developer-Blog/static/js/main.32808ce7.chunk.js"
+  },
+  {
+    "revision": "d0c49cc6f44526ca6efa",
+    "url": "/Developer-Blog/static/js/runtime-main.2dafda56.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Developer-Blog/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Developer-Blog/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Developer-Blog/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Developer-Blog/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Developer-Blog/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
